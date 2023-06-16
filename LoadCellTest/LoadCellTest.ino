0219c30e6c3baf6704472b51202f85a7f35fbb6b
Utensile <132955875+Utensile@users.noreply.github.com>
2023-06-16 17:51:49 +0200

    Update LoadCellTest.ino

--- LoadCellTest/LoadCellTest.ino
+++ LoadCellTest/LoadCellTest.ino
@@ -4,25 +4,20 @@
 
 
 float i=0.0;
-long t=-9999;
 
 void setup() {
   Serial.begin(SerialRate);
-  Serial.println("Communication Begun");  
 }
  
 void loop() {
-  if(millis()-t>100){
-    Serial.print(i);
-    Serial.print(" ");
-    Serial.println(millis());
-    if(millis()%24000<3000){
-      i*=1.00+float(random(0,10))/100;
-      i+=random(0, 100);
-    }
-    else
-      i*=1/(1.00+float(random(0,5))/100);
-    t=millis();
+  Serial.print(i);
+  Serial.print(" ");
+  Serial.println(millis());
+  if(millis()%24000<3000){
+    i*=1.00+float(random(0,10))/100;
+    i+=random(0, 100);
   }
-
+  else
+    i*=1/(1.00+float(random(0,5))/100);
+  delay(100);
 }
